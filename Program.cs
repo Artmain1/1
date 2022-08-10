ce574e899c4a2b74c2fb051acc7cd7eb42b18ef2
Artur <89503357000@ya.ru>
2022-08-10 12:00:20 +0600

    9870

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Write("Введите имя пользователя:");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша")
+if (username.ToLower() == "Маша")
 {
     Console.WriteLine("Ура это же маша");
 }
-else 
+else
 {
     Console.Write("Привет, ");
     Console.WriteLine(username);
